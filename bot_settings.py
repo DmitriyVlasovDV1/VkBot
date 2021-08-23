044d4a726b7a485c569f9020b155d2ef94b9cafe
VlasovDmitriy <vlasov.dmitriy.dv1@gmail.com>
2021-08-23 12:58:35 +0300

    DONE: fix fault

--- bot_settings.py
+++
@@ -1,8 +0,0 @@
-ACCESS_TOKEN = '73ec314b1c401c4b7f64385f4b26a74cc586bdda6a8e2d1207e3376a2b95e2613681470408609c8ec3deb'
-CONFIRMATION_TOKEN = 'd9f2690c'
-
-DB_HOST = 'vlasovl7.beget.tech'
-DB_USER = 'vlasovl7_db'
-DB_PASSWORD = 'RzuDjPk9'
-DB_NAME = 'vlasovl7_db'
-
