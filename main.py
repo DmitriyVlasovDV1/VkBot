3ef8c4acca9730d09aa1171beed68e3b8857b368
VlasovDmitriy <vlasov.dmitriy.dv1@gmail.com>
2021-08-29 19:00:23 +0300

    DONE: Header of site

--- main.py
+++ main.py
@@ -1,40 +1,20 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask, request, json, render_template, session, flash
+from flask.sessions import NullSession
 import os
 
-from flask.sessions import NullSession
-
+from database import db
 from response_handler import response_handler
-from database import database
-
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-db = database()
-reshand = response_handler(db)
+res_hand = response_handler(db)
 
-links = [{'path' : '/bot_debug', 'text' : 'Bot debug'},
-         {'path' : '/information', 'text' : 'Information'},
-         {'path' : '/statistics', 'text' : 'Statistics'}
-]
-
-messages = [{'author' : 'bot', 'text': 'Im Bot'},
-{'author' : 'user', 'text': 'Im User'},
-{'author' : 'bot', 'text': 'Hello\nHello!!!!'},
-{'author' : 'user', 'text': 'Hiffffffffff, bot'},
-] * 5
-
-debug_users = [{'name' : 'Anton', 'id' : 23123},
-               {'name' : 'Anton4', 'id' : 23123},
-               {'name' : 'Antdfso5n', 'id' : 23123},
-               {'name' : 'Adsfnto5n', 'id' : 23123},
-               {'name' : 'Anasasdto5n', 'id' : 23123}]
-
-
-@app.route('/bot_debug', methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def bot_debug():
-    
+
+    return render_template('index.html')
 
     if request.method == 'POST':
         if 'selected_user' in request.form:
@@ -50,7 +30,7 @@ def bot_debug():
             message = request.form['message_new']
             debug_user = db.debug_user_get(user_name)
             db.debug_messages_add(user_name, 'user', message)
-            reshand.run_session({'user_id' : debug_user['user_id'], 'body' : message})
+            res_hand.run_session({'user_id' : debug_user['user_id'], 'body' : message})
 
     debug_users = db.debug_users_get()
     if 'current_user' in session:
@@ -65,31 +45,3 @@ def bot_debug():
         debug_users=debug_users,
         current_user=current_user)
 
-@app.route('/')
-def main_get():
-    db = database()
-    reshand = response_handler()
-
-    if request.method == 'POST':
-        return 'ok'
-    return render_template('index.html', title='Index', links = links)
-
-#@app.route('/', methods=['POST'])
-'''
-@app.route('/', methods=['GET'])
-def processing():
-    #Распаковываем json из пришедшего POST-запроса
-    data = json.loads(request.data)
-
-    if 'type' not in data.keys():
-        return 'not ok'
-    if data['type'] == 'confirmation':
-        return bot_settings.CONFIRMATION_TOKEN
-    if 'object' not in data.keys():
-        return 'not ok'
-
-    #if data['type'] == 'message_new':
-        #reshand.run_session(data['object'])
-
-    return 'ok'
-'''
